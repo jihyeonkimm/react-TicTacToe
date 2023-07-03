3a7b50fde82194d34eff9621d8e2f8c2a2528bc9
jihyeonkimm <jh418531@gmail.com>
2023-07-03 09:07:07 +0900

    리액트 기본 구조 설치

---
+++ src/App.js
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <div className="game">
+      <div className="game-board">
+
+      </div>
+      <div className="game-info">
+        
+      </div>
+    </div>
+  );
+}
+
+export default App;
