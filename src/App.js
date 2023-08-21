67cd7e0b574e0db1a6d4a129f27f179468c9f14e
jihyeonkimm <jh418531@gmail.com>
2023-08-21 10:54:00 +0900

    Board, Square 컴포넌트 생성하기

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
+import Board from "./components/Board";
+
 function App() {
   return (
     <div className="game">
       <div className="game-board">
-
+        <Board />
       </div>
       <div className="game-info">
         
